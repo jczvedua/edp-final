6fcd549b543e67f7ed1b882a549f6ce251972630
jczvedua <143686791+jczvedua@users.noreply.github.com>
2025-04-29 02:35:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.getElementById("logo").onclick = function() {
+    location.href = "home.html";
+};
+
+document.getElementById("alumni").onclick = function() {
+    location.href = "alumni.html";
+};
+
+document.getElementById("abm").onclick = function() {
+    location.href = "programs/abm.html";
+};
+
+document.getElementById("health").onclick = function() {
+    location.href = "programs/health.html";
+};
+
+document.getElementById("humss").onclick = function() {
+    location.href = "programs/humss.html";
+};
+
+document.getElementById("arts").onclick = function() {
+    location.href = "programs/arts.html";
+};
+
+document.getElementById("pe").onclick = function() {
+    location.href = "programs/pe.html";
+};
+
+document.getElementById("sacred_sciences").onclick = function() {
+    location.href = "programs/sacred_sciences.html";
+};
+
+document.getElementById("stem").onclick = function() {
+    location.href = "programs/stem.html";
+};
+
